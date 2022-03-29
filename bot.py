35f91441f7b857aad7bbb3daee53c22b7733ed38
CaDiBob <93527781+CaDiBob@users.noreply.github.com>
2022-03-29 18:14:02 +0900

    Добавил эхобот

---
+++ bot.py
@@ -0,0 +1,116 @@
+import os
+import logging
+import redis
+from environs import Env
+
+import telegram
+from telegram.ext import Filters, Updater
+from telegram.ext import CallbackQueryHandler, CommandHandler, MessageHandler
+
+_database = None
+
+logger = logging.getLogger('bot')
+
+
+class TelegramLogsHandler(logging.Handler):
+
+    def __init__(self, chat_id, bot):
+        super().__init__()
+        self.chat_id = chat_id
+        self.tg_bot = bot
+
+    def emit(self, record):
+        log_entry = self.format(record)
+        self.tg_bot.send_message(chat_id=self.chat_id, text=log_entry)
+
+
+def start(bot, update):
+    """
+    Хэндлер для состояния START.
+
+    Бот отвечает пользователю фразой "Привет!" и переводит его в состояние ECHO.
+    Теперь в ответ на его команды будет запускаеться хэндлер echo.
+    """
+    update.message.reply_text(text='Привет!')
+    return "ECHO"
+
+
+def echo(bot, update):
+    """
+    Хэндлер для состояния ECHO.
+
+    Бот отвечает пользователю тем же, что пользователь ему написал.
+    Оставляет пользователя в состоянии ECHO.
+    """
+    users_reply = update.message.text
+    update.message.reply_text(users_reply)
+    return "ECHO"
+
+
+def handle_users_reply(bot, update):
+    """
+    Функция, которая запускается при любом сообщении от пользователя и решает как его обработать.
+    Эта функция запускается в ответ на эти действия пользователя:
+        * Нажатие на inline-кнопку в боте
+        * Отправка сообщения боту
+        * Отправка команды боту
+    Она получает стейт пользователя из базы данных и запускает соответствующую функцию-обработчик (хэндлер).
+    Функция-обработчик возвращает следующее состояние, которое записывается в базу данных.
+    Если пользователь только начал пользоваться ботом, Telegram форсит его написать "/start",
+    поэтому по этой фразе выставляется стартовое состояние.
+    Если пользователь захочет начать общение с ботом заново, он также может воспользоваться этой командой.
+    """
+    db = get_database_connection()
+    if update.message:
+        user_reply = update.message.text
+        chat_id = update.message.chat_id
+    elif update.callback_query:
+        user_reply = update.callback_query.data
+        chat_id = update.callback_query.message.chat_id
+    else:
+        return
+    if user_reply == '/start':
+        user_state = 'START'
+    else:
+        user_state = db.get(chat_id).decode("utf-8")
+
+    states_functions = {
+        'START': start,
+        'ECHO': echo
+    }
+    state_handler = states_functions[user_state]
+    next_state = state_handler(bot, update)
+    db.set(chat_id, next_state)
+
+
+def get_database_connection():
+    """
+    Возвращает конекшн с базой данных Redis, либо создаёт новый, если он ещё не создан.
+    """
+    global _database
+    if _database is None:
+        database_password = env('REDIS_PASSWORD')
+        database_host = env('REDIS_HOST')
+        database_port = env('REDIS_PORT')
+        _database = redis.Redis(host=database_host, port=database_port, password=database_password)
+    return _database
+
+
+def error(update, context):
+    logger.error(context.error)
+
+
+if __name__ == '__main__':
+    env = Env()
+    env.read_env()
+    tg_token = env("TG_TOKEN")
+    tg_chat_id = env('TG_CHAT_ID')
+    updater = Updater(tg_token)
+    bot = telegram.Bot(token=tg_token)
+    logger.addHandler(TelegramLogsHandler(tg_chat_id, bot))
+    dispatcher = updater.dispatcher
+    dispatcher.add_handler(CallbackQueryHandler(handle_users_reply))
+    dispatcher.add_handler(MessageHandler(Filters.text, handle_users_reply))
+    dispatcher.add_handler(CommandHandler('start', handle_users_reply))
+    dispatcher.add_error_handler(error)
+    updater.start_polling()

2d1e94629e2d3f24845fd34e3143de1fcf3e301a
CaDiBob <93527781+CaDiBob@users.noreply.github.com>
2022-03-29 16:17:02 +0900

    Добавил товар в корзину

--- main.py
+++ main.py
@@ -3,6 +3,24 @@ from environs import Env
 from pprint import pprint
 
 
+def add_to_cart(access_token):
+    url = 'https://api.moltin.com/v2/carts/:reference/items'
+    headers = {
+        'Authorization': f'Bearer {access_token}',
+    }
+    json_data = {
+        'data': {
+            'id': 'a08f9596-ec79-4663-bce7-c25e55df66de',
+            'type': 'cart_item',
+            'quantity': 1,
+        },
+    }
+    response = requests.post(url, headers=headers, json=json_data)
+    response.raise_for_status()
+    answer = response.json()
+    return answer
+
+
 def get_moltin_access_token(client_id):
     url = 'https://api.moltin.com/oauth/access_token'
     data = {
@@ -31,7 +49,7 @@ def main():
     env.read_env()
     client_id = env('MOLTIN_CLIENT_ID')
     access_token = get_moltin_access_token(client_id)
-    pprint(get_products(access_token))
+    pprint(add_to_cart(access_token))
 
 
 if __name__ == '__main__':

be4c5ec69e6fc596ace0b947128e536c9cf6adea
CaDiBob <nikolay.v.byankin@gmail.com>
2022-04-16 23:22:20 +0900

    Добавил удаление товаров из корзины

--- bot.py
+++ bot.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python
+from unicodedata import name
 import redis
 import telegram
 from environs import Env
@@ -15,19 +16,21 @@ from telegram.ext import (
 )
 
 from moltin import (
-    put_product_to_cart,
     create_cart,
     get_products,
     get_moltin_access_token,
     get_product_detail,
     get_product_info,
-    get_cart_info,
+    get_cart_products,
+    get_cart_info_products,
     get_cart_sum,
     get_img,
+    put_product_to_cart,
+    remove_cart_item,
 )
 
 
-HANDLE_MENU, HANDLE_DESCRIPTION = range(2)
+HANDLE_MENU, HANDLE_DESCRIPTION, HANDLE_CART = range(3)
 
 
 def handle_menu(update, context):
@@ -100,18 +103,57 @@ def cart_info(update, context):
     user_id = update.effective_user.id
     access_token = context.bot_data['access_token']
     cart_id = context.user_data['cart_id']
-    cart_info = get_cart_info(access_token, cart_id)
+    cart_products = get_cart_products(access_token, cart_id)
+    cart_info = get_cart_info_products(cart_products)
     cart_sum = get_cart_sum(access_token, cart_id)
+    keyboard = list()
+    for product in cart_products:
+        title = product['name']
+        context.user_data['title'] = title
+        product_id = product['id']
+        keyboard.append(
+            [
+                InlineKeyboardButton(
+                    f'Удалить из корзины {title}',
+                    callback_data=product_id,
+                )
+            ]
+        )
+    keyboard.append(
+        [
+            InlineKeyboardButton(
+                'В меню',
+                callback_data='В меню',
+            )
+        ]
+    )
+    reply_markup = InlineKeyboardMarkup(keyboard)
     bot.send_message(
         text=f'{cart_info}\n{cart_sum}',
         chat_id=user_id,
+        reply_markup=reply_markup,
     )
+    return HANDLE_CART
 
 
-def button(update, context):
-    query = update.callback_query
-    query.answer()
-    query.edit_message_text(text=f"Selected option: {query.data}")
+def remove_item(update, context):
+    bot = context.bot
+    user_id = update.effective_user.id
+    access_token = context.bot_data['access_token']
+    cart_id = context.user_data['cart_id']
+    product_id = update.callback_query.data
+    title = context.user_data['title']
+    keyboard = [
+        [InlineKeyboardButton('Корзина', callback_data='Корзина')]
+    ]
+    remove_cart_item(access_token, cart_id, product_id)
+    reply_markup = InlineKeyboardMarkup(keyboard)
+    bot.send_message(
+        text=f'Товар {title} удален из корзины',
+        chat_id=user_id,
+        reply_markup=reply_markup,
+    )
+    return HANDLE_CART
 
 
 def main() -> None:
@@ -144,7 +186,12 @@ def main() -> None:
                 CallbackQueryHandler(handle_menu, pattern=r'Назад'),
                 CallbackQueryHandler(add_product_to_cart, pattern=r'[0-9]'),
                 CallbackQueryHandler(hendle_description),
-            ]
+            ],
+            HANDLE_CART: [
+                CallbackQueryHandler(cart_info, pattern=r'Корзина'),
+                CallbackQueryHandler(handle_menu, pattern=r'В меню'),
+                CallbackQueryHandler(remove_item),
+            ],
         },
         fallbacks=[],
         # name="my_conversation",
--- moltin.py
+++ moltin.py
@@ -3,7 +3,18 @@ from environs import Env
 from pprint import pprint
 
 
-def get_cart_info(access_token, cart_id):
+def remove_cart_item(access_token, cart_id, product_id):
+    url = f'https://api.moltin.com/v2/carts/{cart_id}/items/{product_id}'
+    headers = {
+        'Authorization': f'Bearer {access_token}',
+    }
+    response = requests.delete(url, headers=headers)
+    response.raise_for_status()
+    answer = response.json()
+    return answer
+
+
+def get_cart_products(access_token, cart_id):
     url = f'https://api.moltin.com/v2/carts/{cart_id}/items'
     headers = {
         'Authorization': f'Bearer {access_token}',
@@ -11,11 +22,10 @@ def get_cart_info(access_token, cart_id):
     response = requests.get(url, headers=headers)
     response.raise_for_status()
     answer = response.json()['data']
-    products_info = get_product_cart_info(answer)
-    return products_info
+    return answer
 
 
-def get_product_cart_info(products):
+def get_cart_info_products(products):
     products_info = str()
     for product in products:
         title = product['name']
@@ -136,17 +146,25 @@ if __name__ == '__main__':
     env.read_env()
     client_id = env('MOLTIN_CLIENT_ID')
     access_token = get_moltin_access_token(client_id)
+    pprint(
+        remove_cart_item(
+            access_token,
+            cart_id='e40bacb2-d8b1-44c2-89bd-2ae3ff78d0b8',
+            product_id='f21a14f5-f796-4606-9d82-e50fcbd3bb45',
+            )
+    )
+    # pprint(
+    #     get_cart_products(
+    #         access_token,
+    #         cart_id='4c572813-70ce-43da-bfed-e6fb965130d0',
+    #         )
+    # )
     # pprint(put_product_to_cart(
     #     access_token,
     #     cart_id='4c572813-70ce-43da-bfed-e6fb965130d0',
     #     product_id='f21a14f5-f796-4606-9d82-e50fcbd3bb45',
     #     quantity=10)
     # )
-    pprint(get_cart_info(
-        access_token,
-        cart_id='4c572813-70ce-43da-bfed-e6fb965130d0',
-    )
-    )
     # pprint(get_cart_sum(
     #     access_token,
     #     cart_id='4c572813-70ce-43da-bfed-e6fb965130d0',
